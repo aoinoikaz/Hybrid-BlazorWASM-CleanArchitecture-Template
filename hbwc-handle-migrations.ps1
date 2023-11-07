9fba053bf3bb8939abafe0b095f71216fcfb7dfc
aoinoikaz <devontomlin97@gmail.com>
2023-11-07 10:07:05 -0500

    adjustment, try again

--- hbwc-handle-migrations.ps1
+++ hbwc-handle-migrations.ps1
@@ -20,23 +20,20 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace $($projectName).Infrastructure.Persistence.Migrations;
 
-public partial class $migrationName : Migration
+public partial class AddPaginationStoredProcedure : Migration
 {
     protected override void Up(MigrationBuilder migrationBuilder)
     {
         var sql = @"
-            
-SET ANSI_NULLS ON
-
-SET QUOTED_IDENTIFIER ON
-
-CREATE OR ALTER PROCEDURE [dbo].[GenericPaginator]
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GenericPaginator]') AND type in (N'P', N'PC'))
+BEGIN
+    EXEC('CREATE PROCEDURE [dbo].[GenericPaginator]
     @TableName NVARCHAR(128),
     @SelectColumns NVARCHAR(MAX),
     @WhereCondition NVARCHAR(MAX) = NULL,
     @JoinCondition NVARCHAR(MAX) = NULL,
-    @OrderByColumn NVARCHAR(128) = 'ID',
-    @OrderByDirection NVARCHAR(4) = 'ASC',
+    @OrderByColumn NVARCHAR(128) = ''ID'',
+    @OrderByDirection NVARCHAR(4) = ''ASC'',
     @PageNumber INT = 1,
     @PageSize INT = 10,
     @FetchTotalCount BIT = 0,
@@ -44,25 +41,21 @@ CREATE OR ALTER PROCEDURE [dbo].[GenericPaginator]
     @TotalCountOut INT OUTPUT
 AS
 BEGIN
-    -- Disable count of rows affected message
     SET NOCOUNT ON;
     
-    -- Declare variables
     DECLARE @SqlQuery NVARCHAR(MAX);
     DECLARE @Offset INT = (@PageNumber - 1) * @PageSize;
     
-    -- Initialize dynamic SQL query
-    SET @SqlQuery = 'SELECT ' + @SelectColumns + 
-                    ' FROM ' + QUOTENAME(@TableName) +
-                    ISNULL(' WITH (INDEX(' + @KnownIndexes + ')) ', '') +
-                    ISNULL(@JoinCondition, '') +
-                    ' WHERE 1=1 ' + 
-                    ISNULL(' AND (' + @WhereCondition + ')', '') +
-                    ' ORDER BY ' + QUOTENAME(@OrderByColumn) + ' ' + @OrderByDirection +
-                    ' OFFSET ' + CAST(@Offset AS NVARCHAR(10)) + ' ROWS' +
-                    ' FETCH NEXT ' + CAST(@PageSize AS NVARCHAR(10)) + ' ROWS ONLY';
+    SET @SqlQuery = ''SELECT '' + @SelectColumns + 
+                    '' FROM '' + QUOTENAME(@TableName) +
+                    ISNULL('' WITH (INDEX('' + @KnownIndexes + '')) '', '''') +
+                    ISNULL(@JoinCondition, '''') +
+                    '' WHERE 1=1 '' + 
+                    ISNULL('' AND ('' + @WhereCondition + '')'', '''') +
+                    '' ORDER BY '' + QUOTENAME(@OrderByColumn) + '' '' + @OrderByDirection +
+                    '' OFFSET '' + CAST(@Offset AS NVARCHAR(10)) + '' ROWS'' +
+                    '' FETCH NEXT '' + CAST(@PageSize AS NVARCHAR(10)) + '' ROWS ONLY'';
     
-    -- Try executing the main query and capture any errors
     BEGIN TRY
         EXEC sp_executesql @SqlQuery;
     END TRY
@@ -70,17 +63,15 @@ BEGIN
         THROW;
     END CATCH
     
-    -- Conditionally fetch the total count
     IF @FetchTotalCount = 1
     BEGIN
-        SET @SqlQuery = 'SELECT @TotalCountOut = COUNT(*) FROM ' + QUOTENAME(@TableName) +
-                       ISNULL(@JoinCondition, '') +
-                       ' WHERE 1=1 ' + 
-                       ISNULL(' AND (' + @WhereCondition + ')', '');
+        SET @SqlQuery = ''SELECT @TotalCountOut = COUNT(*) FROM '' + QUOTENAME(@TableName) +
+                        ISNULL(@JoinCondition, '''') +
+                        '' WHERE 1=1 '' + 
+                        ISNULL('' AND ('' + @WhereCondition + '')'', '''');
 
-        -- Try executing the total count query and capture any errors
         BEGIN TRY
-            EXEC sp_executesql @SqlQuery, N'@TotalCountOut INT OUTPUT', @TotalCountOut=@TotalCountOut OUTPUT;
+            EXEC sp_executesql @SqlQuery, N''@TotalCountOut INT OUTPUT'', @TotalCountOut=@TotalCountOut OUTPUT;
         END TRY
         BEGIN CATCH
             THROW;
@@ -88,11 +79,10 @@ BEGIN
     END
     ELSE
     BEGIN
-	-- Indicator that total count was not fetched
         SET @TotalCountOut = -1; 
     END
-END
-";
+END')
+END";
         migrationBuilder.Sql(sql);
     }
 
